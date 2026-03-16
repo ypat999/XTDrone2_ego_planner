a1e4d323770192bc81f0b6fa56f7ca5aa3f52706
ywj <ypat999@gmail.com>
2026-03-16 18:38:04 +0800

    添加lubancat配置

--- lio/global_config/global_config/__init__.py
+++ lio/global_config/global_config/__init__.py
@@ -135,6 +135,32 @@ config_by_machine = {
         # DESKTOP-ypat主机配置 - Livox MID360
         'LIVOX_MID360_CONFIG': '/home/ywj/dog_slam/LIO-SAM_MID360_ROS2_PKG/ros2/src/livox_ros_driver2/config/MID360_config_gazebo.json',
         'LIVOX_MID360_CONFIG_NO_TILT': '/home/ywj/dog_slam/LIO-SAM_MID360_ROS2_PKG/ros2/src/livox_ros_driver2/config/MID360_config_gazebo.json',
+    },
+    'lubancat': {
+        # lubancat主机配置 - LIO-SAM
+        'ONLINE_LIDAR': False,
+        'LIO_SAM_BASE_CODE_PATH': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/LIO-SAM_MID360_ROS2_DOG/',
+        'DEFAULT_BAG_PATH': '/home/cat/slam_data/livox_record_new/',
+        'DEFAULT_RELIABILITY_OVERRIDE': '/home/cat/slam_data/reliability_override.yaml',
+        'LIO_SAM_DEFAULT_LOAM_SAVE_DIR': '/home/cat/slam_data/loam/',
+
+        # lubancat主机配置 - Nav2
+        'NAV2_BASE_CODE_PATH': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/nav2_dog_slam/',
+        'NAV2_DEFAULT_MAP_FILE': "/home/cat/slam_data/grid_map/map.yaml",
+        'NAV2_DEFAULT_WEB_SCRIPT_PATH': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/nav2_dog_slam/web/run_web.sh',
+        'NAV2_DEFAULT_BT_XML_PATH': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/nav2_dog_slam/config/navigate_to_pose_w_replanning_and_recovery.xml',
+        'NAV2_DEFAULT_PARAMS_FILE': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/nav2_dog_slam/config/nav2_params.yaml',
+        
+        # lubancat主机配置 - FAST-LIO
+        'FAST_LIO_BASE_CODE_PATH': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/FAST_LIO_ROS2_edit/',
+        'FAST_LIO_LIDAR_TYPE': 1,  # 其他主机lidar_type为1
+        'FAST_LIO_MAP_FILE_PATH': '/home/cat/slam_data/pcd/test.pcd',  # 添加的地图文件路径
+        'DEFAULT_RELIABILITY_OVERRIDE': '/home/cat/slam_data/reliability_override.yaml',
+        'DEFAULT_USE_SIM_TIME': False,
+        
+        # lubancat主机配置 - Livox MID360
+        'LIVOX_MID360_CONFIG': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/livox_ros_driver2/config/MID360_config.json',
+        'LIVOX_MID360_CONFIG_NO_TILT': '/home/cat/git/xtd2_ws/XTDrone2_ego_planner/lio/livox_ros_driver2/config/MID360_config.json',
     }
 }
 
