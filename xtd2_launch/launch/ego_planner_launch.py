7c6c7248e48954ba20e8727e61476f918004298c
ypat <ypat_999@163.com>
2026-04-10 17:57:59 +0800

    存一版参数

--- xtd2_launch/launch/ego_planner_launch.py
+++ xtd2_launch/launch/ego_planner_launch.py
@@ -129,7 +129,7 @@ def generate_launch_description():
             {'grid_map/local_update_range_z': 8.0},  # 局部更新范围Z   8.0  
             {'grid_map/obstacles_inflation': 0.4},  # 障碍物膨胀半径 0.6
             {'grid_map/local_map_margin': 2},  # 局部地图边界
-            {'grid_map/ground_height': -0.1},  # 地面高度
+            {'grid_map/ground_height': 0.2},  # 地面高度
 
             # depth filter - 深度滤波器参数
             {'grid_map/use_depth_filter': False},  # 启用深度滤波
